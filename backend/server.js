bc497d10dc15daf460232d3dacd84ed22703656e
PriyankaPandiyan <priyankasuresh857@gmail.com>
2025-11-17 15:45:06 +0530

    Fixed root route

--- backend/server.js
+++ backend/server.js
@@ -9,19 +9,23 @@ import carRoutes from "./routes/carRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
 import employeeRoutes from "./routes/employeeRoutes.js";
-
 import uploadRoute from "./routes/uploadRoute.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(path.resolve(), "/uploads")));
 
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Car FSD Backend Running Successfully 🚀");
+});
 
-// Routes
+// API Routes
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/cars", carRoutes);
@@ -29,8 +33,7 @@ app.use("/api/bookings", bookingRoutes);
 app.use("/api/employees", employeeRoutes);
 app.use("/api/upload", uploadRoute);
 
-
-// Error middleware
+// Error Middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
